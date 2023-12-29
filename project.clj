c599dca9c8101d5dadd919594a9058461afd4db2
Vladimir Legkunets <vladimir.legkunets@gmail.com>
2023-12-29 15:58:45 +0000

    Fixing bad figwheel behavior for m1

--- project.clj
+++ project.clj
@@ -52,7 +52,8 @@
              ;; :server-port 3449 ;; default
              ;; :server-ip "127.0.0.1"
 
-             :css-dirs ["resources/public/css"]} ;; watch and update CSS
+             :css-dirs ["resources/public/css"] ;; watch and update CSS
+             :hawk-options {:watcher :polling}} ;;https://github.com/bhauman/figwheel-main/issues/253
 
              ;; Start an nREPL server into the running figwheel process
              ;; :nrepl-port 7888
